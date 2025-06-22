4333e794805ede98bd6cd6b73964f34895b4c867
matthiassouthwick <southwickmatthias@gmail.com>
2025-06-22 10:10:39 -0600

    Update getAssets.sh

--- getAssets.sh
+++ getAssets.sh
@@ -1,3 +1,3 @@
 rm -rf site/assets
 mkdir site/assets
-gdrive files list --parent $1 --skip-header --field-separator , | cut -d , -f 1 | xargs -I{} gdrive files download {} --destination site/assets
+gdrive files list --parent $1 --max 500 --skip-header --field-separator , | cut -d , -f 1 | xargs -I{} gdrive files download {} --destination site/assets
